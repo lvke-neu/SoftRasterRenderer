753716d78235b97652307ff0a93f8946c7af09d9
ke.lv <lvke>
2024-02-21 11:41:29 +0800

    OpenCV

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
+#include <opencv2/opencv.hpp>
+
 int main()
 {
-    return;
+
+	cv::Mat image(700, 700, CV_32FC3);
+	//image.convertTo(image, CV_8UC3, 1.0f);
+	cv::imshow("image", image);
+	while (true);
+    return 0;
 }
